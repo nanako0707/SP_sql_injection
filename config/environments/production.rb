4a8a3ee4c35a333fb63a3ee900431cbd29655278
Nanako Sawamura <n.k.0213.0529@gmail.com>
2021-11-08 12:47:16 +0900

    rename app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "brakeman_production"
+  # config.active_job.queue_name_prefix = "sql_injection_production"
 
   config.action_mailer.perform_caching = false
 
